4e30b6b8be68df69901c2ff70680f773e6446447
ANDREW SEGAL <segal.a@hotmail.com>
2018-05-02 14:28:45 -0400

    added sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,19 @@
 class SessionsController < ApplicationController
   def create
-      @user = User.find_by(name: params[:name])
+    @user = User.find_by(name: params[:name])
 
-      if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to root_path
-      else
-        flash.now[:alert] = ["Login failed, name and/or password are incorrect"]
-        redirect_to root_path
-      end
-    end
-
-    def destroy
-      session[:user_id] = nil
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash.now[:alert] = ["Login failed, name and/or password are incorrect"]
       redirect_to root_path
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
 end
